072eeab39389ab0f358d1141881575c9c997e5c4
Edwin Kimsal <kimsal.edwin@gmail.com>
2025-05-24 17:12:53 -0400

    Add files via upload

---
+++ spawn.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class spawn : MonoBehaviour
+{
+    // Create public class to spawn obj
+    public GameObject circle;
+
+    // Random class
+    Random rnd = new Random();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Spawn two balls when game is started
+        spawn_ball(400, 200);
+        spawn_ball(-400, -200);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    } 
+
+    // Spawn ball method
+    void spawn_ball(int x, int y){
+        // Set random velocity of circle
+        int vel_x = rnd.Next(-50, 51);
+        int vel_y = rnd.Next(-50, 51);
+
+        // Add circle to scene as ball and create rigidbody
+        GameObject ball = Instantiate(circle, new Vector3(x, y), Quaternion.identity);
+        ball.GetComponent<Rigidbody>().velocity = new Vector3(vel_x, vel_y);
+    }
+}
---
+++ wall_bounce.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class wall_bounce : MonoBehaviour
+{
+    // Get border and wall objects
+    public GameObject front;
+
+    // Create private variables used in initilization
+    private float mag_x;
+    private float mag_y;
+    private float ball_radius;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the magnitude the edge is from point 0
+        mag_x = front.transform.localScale.x/2;
+        mag_y = front.transform.localScale.y/2;
+
+        // Get radius of ball
+        ball_radius = transform.localScale.x/2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set self Rigidbody as rb
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        // Get positions of self
+        float pos_x = transform.position.x;
+        float pos_y = transform.position.y;
+
+        // Get velocities of self
+        float vel_x = rb.velocity.x;
+        float vel_y = rb.velocity.y;
+
+        // Check if ball hit the vertical (x) wall
+        if ((Math.Abs(pos_x) + ball_radius) >= mag_x)
+        {
+            rb.velocity = new Vector3(wall_hit(vel_x), vel_y);
+        }
+
+        // Check if ball hit the horizontal (y) wall
+        if ((Math.Abs(pos_y) + ball_radius) >= mag_y)
+        {
+            rb.velocity = new Vector3(vel_x, wall_hit(vel_y));
+        }
+    }
+
+    // Change velocity when ball hits a wall
+    public float wall_hit(float vel)
+    {
+        return vel * -1;
+    }
+}
